609fb13438cdd240e7def5cef1b649ac0bc1ad77
ToanDinh0503 <106976894+ToanDinh0503@users.noreply.github.com>
2024-08-09 18:21:49 +0700

    package

--- server.js
+++ server.js
@@ -1,32 +1,29 @@
-const express = require("express");
-const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
-const { v4: uuidV4 } = require("uuid");
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-app.get("/", (req, res) => {
-  res.redirect(`/${uuidV4()}`);
-});
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
 
-app.get("/:room", (req, res) => {
-  res.render("room", { roomId: req.params.room });
-});
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
 
-io.on("connection", (socket) => {
-  socket.on("join-room", (roomId, userId) => {
-    socket.join(roomId);
-    socket.to(roomId).broadcast.emit("user-connected", userId);
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
 
-    socket.on("disconnect", () => {
-      socket.to(roomId).broadcast.emit("user-disconnected", userId);
-    });
-  });
-});
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
 
-const PORT = process.env.PORT || 3000; // Ensure the server listens on the appropriate port
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+server.listen(3000)
